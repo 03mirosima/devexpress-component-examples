0121c90579543c97eb7fb99ff9c4cfbcf60b13f5
03mirosima <sezenmiray2009@hotmail.com>
2021-02-16 14:01:53 +0300

    initial commit

---
+++ src/components/menu/MenuExample.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Menu from "devextreme-react/menu";
+import axios from "axios";
+import CustomStore from "devextreme/data/custom_store";
+
+const menudata = new CustomStore({
+  key: "id",
+  loadMode: "raw",
+  load: function () {
+    return axios
+      .get(
+        "https://testdinamikoto.yuceyazilim.com.tr/api/Products/GetTopNavBar"
+      )
+      .then((response) => {
+        return console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  },
+});
+
+class MenuExample extends React.Component {
+  render() {
+    return (
+      <Menu
+        dataSource={menudata}
+        displayExpr="title"
+        orientation="horizontal"
+        onItemClick={this.itemClick}
+      />
+    );
+  }
+}
+export default MenuExample;
